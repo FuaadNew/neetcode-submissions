a99be1affcb9f12c7343f1e546a289a492bd19fd
FuaadNew <134631945+FuaadNew@users.noreply.github.com>
2026-06-23 07:45:50 -0500

    Add: pascals-triangle-ii - submission-1

---
+++ Data Structures & Algorithms/pascals-triangle-ii/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        prev_row = [1] #row zero, length 1
+
+
+        for i in range(1,rowIndex + 1):
+            row = [1] * (i + 1) #row i length i + 1
+
+
+            for j in range(1, len(row)-1):
+                row[j] = prev_row[j] + prev_row[j-1]
+            
+            prev_row = row
+        return prev_row
+            
+
+

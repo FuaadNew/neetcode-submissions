a554f6c0d8f6c372c02d233a3fdceacd679c4977
FuaadNew <134631945+FuaadNew@users.noreply.github.com>
2026-06-23 16:57:56 -0500

    Add: is-palindrome - submission-11

---
+++ Data Structures & Algorithms/is-palindrome/submission-11.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        def isalphanumeric(c):
+            return( ord('a') <= ord(c) <= ord('z') 
+            or ord('0') <= ord(c) <= ord('9') )
+
+        l,r = 0,len(s)-1
+        while l <=r:
+            while l <= r and not isalphanumeric(s[l].lower()):
+                l+=1
+            while r >= l and not isalphanumeric(s[r].lower()):
+                r-=1
+            if l >= r:
+                break
+            if s[l].lower()!= s[r].lower():
+                return False
+            l+=1
+            r-=1
+        
+
+        return True

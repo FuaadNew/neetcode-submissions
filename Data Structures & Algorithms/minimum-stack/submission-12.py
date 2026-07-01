8f736b67c5abab344e09602e76064cbdd7199ee3
FuaadNew <134631945+FuaadNew@users.noreply.github.com>
2026-07-01 06:35:09 -0500

    Add: minimum-stack - submission-12

---
+++ Data Structures & Algorithms/minimum-stack/submission-12.py
@@ -0,0 +1,27 @@
+class MinStack:
+
+    def __init__(self):
+        self.min = []
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if not self.min:
+            self.min.append(val)
+        else:
+            if val <= self.min[-1]:
+                self.min.append(val)
+        self.stack.append(val)
+
+
+    def pop(self) -> None:
+        val = self.stack.pop()
+        if val == self.min[-1]:
+            self.min.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.min[-1]

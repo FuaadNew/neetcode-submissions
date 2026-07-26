cb5e65e052a7520d39a37650d21f15c23b85dfee
FuaadNew <134631945+FuaadNew@users.noreply.github.com>
2026-07-26 07:43:23 -0500

    Add: evaluate-division - submission-1

---
+++ Data Structures & Algorithms/evaluate-division/submission-1.py
@@ -0,0 +1,43 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        adj = {}
+
+        for i in range(len(equations)):
+            a,b = equations[i]
+            value = values[i]
+            if a not in adj:
+                adj[a] = []
+            if b not in adj:
+                adj[b] = []
+            
+            adj[a].append((b,value))
+            adj[b].append((a,1/value))
+        print(adj.items())
+        
+        def solve(src, target):
+            stack = deque([(src, 1)])
+            visit = set()
+            while stack:
+                curr,w = stack.popleft()
+                print(curr,target,w)
+                if curr == target:
+                    return w
+                if curr not in adj:
+                    continue
+                if curr in visit:
+                    continue
+                visit.add(curr)
+                for nei,weight in adj[curr]:
+                    stack.append((nei, w * weight))
+            return -1
+        res = [0] * len(queries)
+        for i in range(len(queries)):
+            a,b = queries[i]
+            if a not in adj or b not in adj:
+                res[i]= -1.0
+            else:
+                res[i]= solve(a,b)
+        return res
+
+            
+        

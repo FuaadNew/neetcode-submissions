05124f5d001a3e155b648b8f6762953b96d5d225
FuaadNew <134631945+FuaadNew@users.noreply.github.com>
2026-07-26 08:39:33 -0500

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = {}
+
+        for a,b in prerequisites:
+            if a not in adj:
+                adj[a] = []
+            adj[a].append(b)
+
+        def solve(course):
+            stack = deque([course])
+            visit = set()
+            while stack:
+                curr = stack.popleft()
+                if curr not in adj:
+                    continue
+                if curr in visit:
+                    continue
+                visit.add(curr)
+                for nei in adj[curr]:
+                    if nei == course:
+                        return False
+                    stack.append(nei)
+
+            return True
+        print(adj.items())
+        for i in range(numCourses):
+            if not solve(i):
+                print(i)
+                return False
+        return True

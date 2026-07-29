6134aab344d03e0d4f8e1a8ae4946f073e398fa3
FuaadNew <134631945+FuaadNew@users.noreply.github.com>
2026-07-29 08:27:49 -0500

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.py
@@ -0,0 +1,55 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = {}
+
+        for i in range(len(prerequisites)):
+            a,b = prerequisites[i]
+            if a not in adj:
+                adj[a] = []
+            adj[a].append(b)
+        
+        completed = set()
+        visit = set()
+        res = []
+
+        def solve(i):
+            stack = [(i,False)]
+            path = []
+
+            while stack:
+                curr, returning = stack.pop()
+                if returning:
+                    visit.remove(curr)
+                    completed.add(curr)
+                    path.append(curr)
+                else:
+                    if curr in visit:
+                        return []
+                    if curr in completed:
+                        continue
+                    if curr not in adj:
+                        path.append(curr)
+                        completed.add(curr)
+                        continue
+                    visit.add(curr)
+                    stack.append((curr, True))
+
+                    for nei in adj[curr]:
+                        stack.append((nei, False))
+
+            return path
+
+
+
+        for i in range(numCourses):
+            if i in adj and i not in completed:
+                temp_path = solve(i)
+                if temp_path == []:
+                    return []
+                res.extend(temp_path)
+        
+        for i in range(numCourses):
+            if i not in completed and i not in adj:
+                res.append(i)
+        
+        return res

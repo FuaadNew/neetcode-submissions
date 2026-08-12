250fa2d860e558dbfebecf0b3379aa4468e10fbf
FuaadNew <134631945+FuaadNew@users.noreply.github.com>
2026-08-12 08:30:54 -0500

    Add: search-for-word - submission-18

---
+++ Data Structures & Algorithms/search-for-word/submission-18.py
@@ -0,0 +1,38 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        ROWS,COLS = len(board),len(board[0])
+        counter = defaultdict(int)
+        for r in range(ROWS):
+            for c in range(COLS):
+                counter[board[r][c]]+=1
+        wordCount = Counter(word)
+        for c in wordCount:
+            if c not in counter or  counter[c] < wordCount[c]:
+                return False
+
+
+        def dfs(r,c,i,visit):
+            if i == len(word):
+                return True
+            if (r,c) in visit:
+                return False
+            if r < 0 or r>= ROWS or c < 0 or c >=COLS:
+                return False
+            if word[i] != board[r][c]:
+                return False
+
+            visit.add((r,c))
+
+            left = dfs(r,c - 1,i + 1, visit)
+            right = dfs(r,c + 1,i + 1, visit)
+            up = dfs(r - 1,c,i + 1, visit)
+            down = dfs(r + 1,c,i + 1, visit)
+            res = left or right or up or down
+            visit.remove((r,c))
+            return res
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r,c,0,set()):
+                    return True
+        return False

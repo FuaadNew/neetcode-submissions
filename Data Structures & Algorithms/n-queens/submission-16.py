6145b29f925da0f90d977d2d81febd01991f5f6f
FuaadNew <134631945+FuaadNew@users.noreply.github.com>
2026-08-19 09:26:21 -0500

    Add: n-queens - submission-16

---
+++ Data Structures & Algorithms/n-queens/submission-16.py
@@ -0,0 +1,49 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        
+        def valid(r,c,board):
+            #go right
+            for nc in range(c + 1, n):
+                if board[r][nc] == "Q":
+                    return False
+
+            #go left
+            for nc in range(c - 1, -1,-1):
+                if board[r][nc] == "Q":
+                    return False
+            
+            #go up
+            for nr in range(r - 1, -1,-1):
+                if board[nr][c] == "Q":
+                    return False
+
+            #go diagnol_right
+            i,j = r - 1, c + 1
+            while i >= 0 and j < n:
+                if board[i][j] == "Q":
+                    return False
+                i-=1
+                j+=1
+
+            #go diagnol_left
+            i,j = r - 1, c - 1
+            while i >= 0 and j >= 0:
+                if board[i][j] == "Q":
+                    return False
+                i-=1
+                j-=1
+            return True
+
+        def dfs(r, board):
+            if r == n:
+                res.append(["".join(row) for row in board])
+                return
+            for c in range(n):
+                if valid(r,c,board):
+                    board[r][c] = "Q"
+                    dfs(r + 1, board)
+                    board[r][c] = "."
+        res = []
+        board = [['.'] * n for _ in range(n)]
+        dfs(0, board)
+        return res
